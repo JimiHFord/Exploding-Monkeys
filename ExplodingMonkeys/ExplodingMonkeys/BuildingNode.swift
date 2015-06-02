643dc701d9525fc262ce67dbccb227b1babe2d78
Jimi Ford <jimifordmail@gmail.com>
2015-06-02 19:25:11 -0400

    initial commit

---
+++ ExplodingMonkeys/ExplodingMonkeys/BuildingNode.swift
@@ -0,0 +1,14 @@
+//
+//  BuildingNode.swift
+//  ExplodingMonkeys
+//
+//  Created by James Ford on 6/2/15.
+//  Copyright (c) 2015 JWorks. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class BuildingNode: SKSpriteNode {
+   
+}
